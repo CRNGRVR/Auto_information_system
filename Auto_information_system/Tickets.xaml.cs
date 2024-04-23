7e096188f0b102d0ae6cf90d96be277f9117535c
user <user@COMP-307-008>
2024-04-23 17:15:21 +0700

    Ещё два окна

--- Auto_information_system/Tickets.xaml.cs
+++ Auto_information_system/Tickets.xaml.cs
@@ -23,5 +23,10 @@ namespace Auto_information_system
         {
             InitializeComponent();
         }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
